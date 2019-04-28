e8a57d23e7e49421ee3bfa66f2351334d5895092
Glitch (equable-plane) <none>
2019-04-28 16:22:49 +0000

    🏝💑 Updated with Glitch

--- controllers/formHandler.js
+++ controllers/formHandler.js
@@ -4,12 +4,23 @@ function FormHandler(app, db) {
 
   this.newIssue = function(project, Issue) {
     
-  return new Promise(function(resolve, reject){    
-    db.collection(project).insertOne(Issue, (err, doc) => {
-          if(err) reject(err);
-          resolve(doc);
-        }
-    )});
+    return new Promise(function(resolve, reject){    
+      db.collection(project).insertOne(Issue, (err, doc) => {
+            if(err) reject(err);
+            resolve(doc);
+          }
+      )});
+  }
+  
+  this.updateIssue = function(project, Issue) {
+    
+    var newValues = {$set:Issue};
+    return new Promise(function(resolve, reject){    
+      db.collection(project).updateOne({_id:Issue._id}, newValues,{new:true}, (err, doc) => {
+            if(err) reject("could not update: " + err);
+            resolve("successfully updated");
+          }
+      )});
   }
   
 }
