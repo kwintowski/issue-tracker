06d2a2619e5cc3b118fbfeed8ef8c443d5f8e2d5
Glitch (equable-plane) <none>
2019-04-29 16:19:05 +0000

    👧🏯 Updated with Glitch

--- controllers/formHandler.js
+++ controllers/formHandler.js
@@ -1,28 +1,39 @@
 'use strict';
 
+var ObjectId = require('mongodb').ObjectID;
+
 function FormHandler(app, db) {
 
   this.newIssue = function(project, Issue) {
     
     return new Promise(function(resolve, reject){    
       db.collection(project).insertOne(Issue, (err, doc) => {
-            if(err) reject(err);
-            resolve(doc);
-          }
-      )});
+        if(err) reject(err);
+        resolve(doc);
+      }
+    )});
   }
   
-  this.updateIssue = function(project, Issue) {
+  this.updateIssue = function(project, id, Issue) {
     
     var newValues = {$set:Issue};
     return new Promise(function(resolve, reject){    
-      db.collection(project).updateOne({_id:Issue._id}, newValues,{new:true}, (err, doc) => {
-            if(err) reject("could not update: " + err);
-            resolve("successfully updated");
-          }
-      )});
+      db.collection(project).updateOne({_id:new ObjectId(id)}, newValues, {new:true}, (err, doc) => {
+        if(err) reject("could not update: " + id);
+        resolve("successfully updated");
+      }
+    )});
   }
   
+  this.deleteIssue = function(project, id) {
+    
+    return new Promise(function(resolve, reject){    
+      db.collection(project).findOneAndDelete({_id:new ObjectId(id)}, (err, doc) => {
+        if(err) reject("could not delete: " + id);
+        resolve("deleted " + id);
+      }
+    )});
+  }  
 }
 
 module.exports = FormHandler;
--- routes/api.js
+++ routes/api.js
@@ -46,7 +46,9 @@ module.exports = function (app,db) {
     .put(function (req, res){
       var project = req.params.project;
       
-      var Issue = { _id:req.body._id };
+      var Issue = {};
+    
+      if(req.body._id) {
         req.body.issue_title==""?null:Issue.issue_title=req.body.issue_title;
         req.body.issue_text==""?null:Issue.issue_text=req.body.issue_text;
         req.body.created_by==""?null:Issue.created_by=req.body.created_by;
@@ -54,11 +56,12 @@ module.exports = function (app,db) {
         req.body.status_text==""?null:Issue.status_text=req.body.status_text,
         req.body.open?Issue.open=false:null;
         Issue.updated_on=(new Date()).toISOString();
-                  
-    console.log(Issue);
+      }
     
-      if(Object.keys(Issue).length>2) {
-        formHandler.updateIssue(project, Issue)
+      console.log(Issue);
+    
+      if(Object.keys(Issue).length>0) {
+        formHandler.updateIssue(project, req.body._id, Issue)
           .then((result)=>{res.json(result);})
           .catch((reject)=>{res.json(reject);}); 
       } else {
@@ -69,6 +72,13 @@ module.exports = function (app,db) {
     .delete(function (req, res){
       var project = req.params.project;
       
-    });
+      if(req.body._id) {
+        formHandler.deleteIssue(project, req.body._id)
+          .then((result)=>{res.json(result);})
+          .catch((reject)=>{res.json(reject);}); 
+      } else {
+        res.json("_id error");
+      }
+    })
     
 };
